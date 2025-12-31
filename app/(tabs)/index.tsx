8eb0aa19dcf9e33b15db73b7ff143948655ea8e4
vivek <codecrafter67@gmail.com>
2025-12-31 22:41:48 +0530

    feat:implement home page popular movies section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import MovieCardItem from '@/components/MovieCardItem'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -7,9 +7,20 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function index() {
   return (
-    <SafeAreaView style={{padding:12}}>
-      <Text>index Page</Text>
-     
+    <SafeAreaView style={{ padding: 12 }}>
+      <Text style={{ fontSize: 20, fontWeight: "semibold",padding:12 }}>Popular Movies</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          flexDirection: "row",
+          paddingHorizontal: 10,
+        }}>
+        <MovieCardItem />
+        <MovieCardItem />
+        <MovieCardItem />
+        <MovieCardItem />
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- components/MovieCardItem.tsx
+++ components/MovieCardItem.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function MovieCardItem() {
   return (
-    <View style={{width:105,height:200,marginBottom:10}}>
+    <View style={{width:105,height:200,marginBottom:10,marginRight:10}}>
       <Text style={{width:"100%",height:"70%",backgroundColor:"red",borderRadius:6}}>
       </Text>
       <Text style={{fontSize:18,fontWeight:"semibold",paddingTop:5,paddingBottom:2}}>Gladiator II</Text>
