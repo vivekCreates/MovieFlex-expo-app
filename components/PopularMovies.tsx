e9214a55c435e996b8bee69dd072087cac9a451a
vivek <codecrafter67@gmail.com>
2025-12-31 23:09:09 +0530

    feat: implement the PopularMovies component

---
+++ components/PopularMovies.tsx
@@ -0,0 +1,22 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import MovieCardItem from './MovieCardItem'
+
+export default function PopularMovies() {
+  return (
+    <View>
+       <Text style={{ fontSize: 20, fontWeight: "600",marginBottom:10,color:"white"}}>Popular Movies</Text>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{
+                flexDirection: "row",
+              }}>
+              <MovieCardItem />
+              <MovieCardItem />
+              <MovieCardItem />
+              <MovieCardItem />
+            </ScrollView>
+    </View>
+  )
+}
