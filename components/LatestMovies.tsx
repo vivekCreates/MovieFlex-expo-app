f9e5eab02f37bb156eb37adff32b394d4a00c20d
vivek <codecrafter67@gmail.com>
2025-12-31 23:10:16 +0530

    feat: implement the LatestMovies section component

---
+++ components/LatestMovies.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MovieCardItem from './MovieCardItem'
+
+export default function LatestMovies() {
+    return (
+        <View>
+            <Text style={{ fontSize: 20, fontWeight: "600", marginBottom: 10, color: "white" }}>Latest Movies</Text>
+            <View style={{ flexDirection: "row", flexWrap: 'wrap' }}>
+                <MovieCardItem />
+                <MovieCardItem />
+                <MovieCardItem />
+                <MovieCardItem />
+                <MovieCardItem />
+            </View>
+        </View>
+    )
+}
