0e4b81f117e1fb38a57092f465a8a6ac70aefdf5
vivek <codecrafter67@gmail.com>
2026-01-01 15:18:15 +0530

    complete bookmark page

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import MovieCardItem from '@/components/MovieCardItem'
 
 export default function bookmark() {
   return (
-    <SafeAreaView style={{backgroundColor:"#030014",height:"100%"}}>
-      <Text style={{color:"white"}}>bookmark page</Text>
+    <SafeAreaView style={{backgroundColor:"#030014",height:"100%",padding:10}}>
+      <Text style={{color:"white",fontSize:25,marginTop:20}}>Bookmark Movies</Text>
+      <View style={{flexDirection:"row",gap:10,marginTop:20,flexWrap:"wrap"}}>
+        <MovieCardItem/>
+        <MovieCardItem/>
+        <MovieCardItem/>
+        <MovieCardItem/>
+      </View>
     </SafeAreaView>
   )
 }
