2b9aed1cecdfb74cd9a233796156533c6098725b
vivek <codecrafter67@gmail.com>
2026-01-01 21:30:52 +0530

    complete ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,6 +13,7 @@ export default function index() {
   return (
     <SafeAreaView style={{ padding: 12,backgroundColor:"#030014" }}>
       <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={{fontSize:40,color:"#AF91FF",fontWeight:"bold",textAlign:"center",marginVertical:20}}>MovieFlex</Text>
       <PopularMovies/>
       <LatestMovies/>
       </ScrollView>
